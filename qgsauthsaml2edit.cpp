28ce9e28c26671e2149594e1ede7226346d76489
Salahov Javid <salahov.cavid@gmail.com>
2017-10-15 20:52:05 +0200

    Added author fields

--- qgsauthsaml2edit.cpp
+++ qgsauthsaml2edit.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+    begin                : October 15, 2017
+    copyright            : (C) 2017 by Secure Dimensions GmbH, Germany
+    author               : Andreas Matheus, Secure Dimensions GmbH
+    email                : am at secure-dimensions dot de
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #include <QStandardItemModel>
 #include <QUrl>
 #include <QNetworkAccessManager>
